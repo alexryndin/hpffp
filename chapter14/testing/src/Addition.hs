8513508d8da006be9e85deee2853b6d9fa96d657
Alex Ryndin <alexnryndin@gmail.com>
2018-10-11 19:02:44 +0300

    add notes and exercises from chapter 14

--- chapter14/testing/src/Addition.hs
+++ chapter14/testing/src/Addition.hs
@@ -1,6 +1,7 @@
 module Addition where
 
 import Test.Hspec
+import Test.QuickCheck
 
 -- main :: IO ()
 -- main = hspec $ do
@@ -17,11 +18,18 @@ main = hspec $ do
       (1 + 1) > (1 :: Integer) `shouldBe` True
     it "2 + 2 is equal to 4" $ do
       ((2 + 2) :: Integer) `shouldBe` 4
+    it "x + 1 is always greater than x" $ do
+      property $ \x -> x + 1 > (x :: Int)
   describe "Rmult" $ do
     it "15 * 3 is 45" $ do
       rmult 15 3 `shouldBe` (45 :: Integer)
     it "22 * 0 is 0" $ do
       rmult 22 0 `shouldBe` (0 :: Integer)
+
+prop_additionGreater :: Int -> Bool
+prop_additionGreater x = x + 1 > x
+runQc :: IO ()
+runQc = quickCheck prop_additionGreater
 
 sayHello :: IO ()
 sayHello = putStrLn "hello!"
@@ -37,3 +45,15 @@ rmult a b | a < 0 && b < 0 =   rmult (-a) (-b)
               go 0 _ = 0
               go a' b' = b' + go (a'-1) b'
               in go a b
+
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+  a <- arbitrary
+  elements [Nothing, Just a]
+-- What QuickCheck actually does
+-- so you get more Just values
+genMaybe' :: Arbitrary a => Gen (Maybe a)
+genMaybe' = do
+  a <- arbitrary
+  frequency [ (1, return Nothing)
+            , (3, return (Just a))]

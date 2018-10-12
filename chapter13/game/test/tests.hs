55707b87190d729a1d287fcbb27c604f2513e557
alex ryndin <alexnryndin@gmail.com>
2018-10-12 21:41:09 +0300

    special for valera

--- chapter13/game/test/tests.hs
+++ chapter13/game/test/tests.hs
@@ -1,9 +1,11 @@
+module Main where
+
 import Test.Hspec
 import Test.QuickCheck
-import qualified Main as M
+import qualified Game as M
 
 main :: IO ()
 main = hspec $ do
   describe "fillInCharacter" $ do
     it "fills 'c' character in word" $ do
-      fillInCharacter (M.Puzzle "myword" [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing] "" 0) 'w' `shouldBe` (M.Puzzle "myword" [Nothing, Nothing, Just 'w', Nothing, Nothing, Nothing] "w" 0)
+      (M.fillInCharacter (M.Puzzle "myword" [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing] "" 0) 'w') `shouldBe` (M.Puzzle "myword" [Nothing, Nothing, Just 'w', Nothing, Nothing, Nothing] "w" 0)
